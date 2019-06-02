149c09bf4deff00df0fee8ea44b89e8ac2b8b2df
sb8887 <30408514+sb8887@users.noreply.github.com>
2019-06-02 11:37:18 +0930

    Initial commit

---
+++ XML_IP.py
@@ -0,0 +1,63 @@
+import os
+from lxml import etree
+import socket
+from itertools import zip_longest
+import csv
+
+
+# script_path = os.getcwd()
+# print(script_path)
+
+file_to_open = os.path.join("template.xml")
+
+f = open(file_to_open)
+
+#Set up some lists
+addresses=[]
+names = []
+rows = [addresses,names]
+
+#import the file
+tree = etree.parse(f)
+
+root = tree.getroot()
+#confirm load
+print(root.attrib)
+
+#grab that IP
+for tcp in tree.xpath("//address"):
+    print (tcp.attrib)
+    x = tcp.attrib["addr"]
+    print (x)
+    addresses.append(x)
+
+#confirm it worked.
+print(addresses)
+
+for y in addresses:
+    print(y)
+    #Attempt get host, writes line - Failed if it didnt work.
+    try:
+        host_name = socket.gethostbyaddr(y)
+        a = host_name[0]
+        names.append(a)
+        print(a)
+        print(host_name)
+    except:
+        names.append("Failed")
+        pass
+
+
+#debug
+print(names)
+print("rows")
+print(rows)
+
+
+#write a CSV
+export_data = zip_longest(*rows, fillvalue = '')
+with open('the_ip.csv', 'w', encoding="ISO-8859-1", newline='') as myfile:
+    wr = csv.writer(myfile)
+    wr.writerow(("IP", "Name"))
+    wr.writerows(export_data)
+myfile.close()
