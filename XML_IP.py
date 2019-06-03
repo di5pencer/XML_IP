1d74494325c439935fc12d6678d03bc9e71e0ef7
sb8887 <30408514+sb8887@users.noreply.github.com>
2019-06-04 00:08:31 +0930

    Added some error handling. Now also loads XML's from the directory its placed in

--- XML_IP.py
+++ XML_IP.py
@@ -5,36 +5,59 @@ from itertools import zip_longest
 import csv
 import sys
 
-#not this one...
-#file_to_open = os.path.join("template.xml")
+cwd = os.getcwd()
+files= os.listdir(cwd)
 
-file_to_open =os.path.join(sys.path[0], 'template.xml')
-
-f = open(file_to_open)
-print(file_to_open)
 
 #Set up some lists
+xmls =[]
+#for outupt CSV
 addresses=[]
 names = []
-rows = [addresses,names]
+input_filename =[]
+rows = [addresses,names,input_filename]
 
-#import the file
-tree = etree.parse(f)
 
-root = tree.getroot()
-#confirm load
-print(root.attrib)
+#loads XML files from directory into list.
+for file in files:
+    if file.endswith(".xml"):
+        xmls.append(file)
+        #print (file)
 
-#grab that IP
-for tcp in tree.xpath("//address"):
-    print (tcp.attrib)
-    x = tcp.attrib["addr"]
-    print (x)
-    addresses.append(x)
+print ("\033[0;35;0m" +"Found these XML's : " +str (xmls) +"\033[0;0m")
 
-#confirm it worked.
-print(addresses)
+for xml in xmls:
+    #import the file
+    try:
+        file_to_open = os.path.join(sys.path[0], xml)
+        f = open(file_to_open)
+        print("\033[0;32;0m" +"Opening this file and loading to list:  " + str(file_to_open) +"\033[0;0m" )
+        tree = etree.parse(f)
+        root = tree.getroot()
+        #confirm load
+        #print(root.attrib)
 
+        # grab that IP
+        for tcp in tree.xpath("//address"):
+            input_filename.append(xml)
+            #print (tcp.attrib)
+            x = tcp.attrib["addr"]
+            #print (x)
+            addresses.append(x)
+    except:
+        #skipping broken files and showing an arror.
+        print("\x1b[1;31;40m" +str(xml) +" Skipped due to error in file \033[0;0m")
+        pass
+
+
+
+
+
+    #confirm it worked.
+#print("The addresses are: " +str(addresses))
+
+
+#Name lookup with sockets
 for y in addresses:
     print(y)
     #Attempt get host, writes line - Failed if it didnt work.
@@ -43,22 +66,25 @@ for y in addresses:
         a = host_name[0]
         names.append(a)
         print(a)
-        print(host_name)
+        #print(host_name)
     except:
         names.append("Failed")
         pass
 
 
 #debug
-print(names)
-print("rows")
-print(rows)
+# print(names)
+# print("rows")
+# print(rows)
 
 
 #write a CSV
+print("")
+print ("Exporting a CSV")
 export_data = zip_longest(*rows, fillvalue = '')
 with open('the_ip.csv', 'w', encoding="ISO-8859-1", newline='') as myfile:
     wr = csv.writer(myfile)
-    wr.writerow(("IP", "Name"))
+    wr.writerow(("IP", "Name", "Input filename"))
     wr.writerows(export_data)
+    print("Export complete")
 myfile.close()
